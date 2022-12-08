6788bc6965e1efad4224746cdbba1cd6171d269d
syahdaromstudenttelu <syahdarom@student.telkomuniversity.ac.id>
2022-12-09 00:40:42 +0700

    chore(project): initialize project basic setup

---
+++ README.md
@@ -0,0 +1,11 @@
+# Tugas Besar Cloud Computing TT-43-G1
+
+## Kelompok 7
+
+Judul aplikasi: Sistem tiket travel dan pembayaran
+
+Nama anggota:
+
+- Fatma Nur Afifah (1101194137)
+- Syahda Romansyah (1101190402)
+- Zahran Nabil (1101190116)
---
+++ src/pages/_document.tsx
@@ -0,0 +1,26 @@
+import type { DocumentContext, DocumentInitialProps } from 'next/document';
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class CustomDocument extends Document {
+  static async getInitialProps(
+    ctx: DocumentContext
+  ): Promise<DocumentInitialProps> {
+    const initialProps = await Document.getInitialProps(ctx);
+
+    return initialProps;
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default CustomDocument;
