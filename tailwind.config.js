3f3ce5ca7a5c3403227a7066737aa44e987d69b4
syahdaromstudenttelu <syahdarom@student.telkomuniversity.ac.id>
2022-12-09 04:53:17 +0700

    chore(config): add bg image for Tailwind CSS classname

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,9 @@ module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
+      backgroundImage: {
+        'hero-bg': 'url(/assets/images/bg1.jpg)',
+      },
       fontFamily: {
         raleway: ['var(--font-raleway)', ...twDefaultTheme.fontFamily.sans],
         inter: ['var(--font-inter)', ...twDefaultTheme.fontFamily.sans],
