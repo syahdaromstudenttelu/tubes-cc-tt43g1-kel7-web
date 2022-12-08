454954527f838fae88680dbc2b57003404243aed
syahdaromstudenttelu <syahdarom@student.telkomuniversity.ac.id>
2022-12-09 05:22:51 +0700

    feat: create button-link components

---
+++ src/components/button-links.ts
@@ -0,0 +1,4 @@
+import PrimaryButtonLink from './button-links/Primary';
+import SecondaryButtonLink from './button-links/Secondary';
+
+export { PrimaryButtonLink, SecondaryButtonLink };
---
+++ src/components/button-links/Secondary.tsx
@@ -0,0 +1,23 @@
+import type { ReactNode } from 'react';
+import type { UrlObject } from 'url';
+import Link from 'next/link';
+import { interFont } from '../../lib/myNextFonts';
+
+interface SecondaryButtonLinkProps {
+  children: ReactNode;
+  hrefPath: string | UrlObject;
+}
+
+export default function SecondaryButtonLink({
+  children,
+  hrefPath,
+}: SecondaryButtonLinkProps) {
+  return (
+    <Link
+      className={`${interFont.variable} inline-block w-full rounded-lg border-4 border-slate-800 py-3 text-center font-inter text-lg font-semibold`}
+      href={hrefPath}
+    >
+      {children}
+    </Link>
+  );
+}
