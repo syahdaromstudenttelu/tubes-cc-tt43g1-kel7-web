a2c44b9257ab47544ff72d53d01841474cf5429a
syahdaromstudenttelu <syahdarom@student.telkomuniversity.ac.id>
2022-12-09 12:52:35 +0700

    feat: add error message component

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -4,11 +4,14 @@ import Head from 'next/head';
 import Link from 'next/link';
 import { PrimaryButton } from '../../components/buttons';
 import { Input } from '../../components/form-inputs';
+import ErrorMessage from '../../components/ErrorMessage';
 import { ralewayFont } from '../../lib/myNextFonts';
 
 export default function Login() {
   const [userEmail, setUserEmail] = useState('');
   const [userPassword, setUserPassword] = useState('');
+  const [showErrorMsg, setShowErrorMsg] = useState(false);
+  const [errorMsg, setErrorMsg] = useState('Maaf, terjadi kesalahan');
   const btnDisabledStatus = userEmail === '' || userPassword === '';
 
   const onEmailInput = (e: ChangeEvent<HTMLInputElement>) => {
@@ -37,13 +40,17 @@ export default function Login() {
               <h2 className="mb-2 cursor-default select-none font-raleway text-4xl font-bold">
                 GOVEL
               </h2>
+
               <p className="mb-2 text-2xl font-bold">Masuk Akun</p>
-              <p>
+
+              <p className="mb-4">
                 Belum memiliki akun?{' '}
                 <Link href="/register" className="text-slate-500 underline">
                   Silakan registrasi
                 </Link>
               </p>
+
+              <ErrorMessage showError={showErrorMsg}>{errorMsg}</ErrorMessage>
             </div>
 
             <div className="mx-auto max-w-2xl">
