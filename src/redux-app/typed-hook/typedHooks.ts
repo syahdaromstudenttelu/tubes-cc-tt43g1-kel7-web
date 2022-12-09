c3f6443c96b5713663c6a110a6f6a8eeda7c3cf4
syahdaromstudenttelu <syahdarom@student.telkomuniversity.ac.id>
2022-12-09 16:24:19 +0700

    refactor: separate import type module with variable module

--- src/redux-app/typed-hook/typedHooks.ts
+++ src/redux-app/typed-hook/typedHooks.ts
@@ -1,5 +1,6 @@
-import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
+import type { TypedUseSelectorHook } from 'react-redux';
 import type { RootState, AppDispatch } from '../store/store';
+import { useDispatch, useSelector } from 'react-redux';
 
 // Use throughout your app instead of plain `useDispatch` and `useSelector`
 export const useAppDispatch: () => AppDispatch = useDispatch;
