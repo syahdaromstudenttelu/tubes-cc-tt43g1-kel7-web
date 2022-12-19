091b38f5f83c0bee24fd2a1c1e931f16d7e621e4
syahdaromstudenttelu <syahdarom@student.telkomuniversity.ac.id>
2022-12-20 00:12:03 +0700

    feat: update register page

--- src/pages/register/index.tsx
+++ src/pages/register/index.tsx
@@ -1,17 +1,25 @@
-import type { ChangeEvent } from 'react';
-import { useState } from 'react';
-import { ralewayFont } from '../../lib/myNextFonts';
+import { FirebaseError } from 'firebase/app';
+import { createUserWithEmailAndPassword, getAuth } from 'firebase/auth';
 import Head from 'next/head';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
+import type { ChangeEvent } from 'react';
+import { useState } from 'react';
+import isEmpty from 'validator/lib/isEmpty';
 import { PrimaryButton } from '../../components/buttons';
-import { Input } from '../../components/form-inputs';
 import ErrorMessage from '../../components/ErrorMessage';
+import { Input } from '../../components/form-inputs';
+import RegisterError from '../../lib/error-class/RegisterError';
+import { ralewayFont } from '../../lib/myNextFonts';
+import { useRedirectHome } from '../../lib/react-custom-hooks/useRedirectHome';
 
 export default function Register() {
   const [userEmail, setUserEmail] = useState('');
   const [userPassword, setUserPassword] = useState('');
   const [showErrorMsg, setShowErrorMsg] = useState(false);
   const [errorMsg, setErrorMsg] = useState('Maaf, terjadi kesalahan');
+  const { userLogin } = useRedirectHome();
+  const nextRouter = useRouter();
   const btnDisabledStatus =
     userEmail === '' || userPassword === '' || userPassword.length < 6;
 
@@ -25,75 +33,147 @@ export default function Register() {
     setUserPassword(() => inputValue);
   };
 
-  const onRegister = () => {};
+  const onRegister = async () => {
+    try {
+      setShowErrorMsg(() => false);
 
-  return (
-    <main className={`${ralewayFont.variable}`}>
-      <Head>
-        <title>GOVEL | Masuk</title>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      </Head>
+      const emailIsEmpty = isEmpty(userEmail, { ignore_whitespace: true });
 
-      <div className="h-screen w-full">
-        <div className="grid h-full items-center">
-          <div>
-            <div className="mb-6 text-center">
-              <h2 className="mb-2 cursor-default select-none font-raleway text-4xl font-bold">
-                GOVEL
-              </h2>
+      if (emailIsEmpty)
+        throw new RegisterError('Mohon isi email Anda dengan benar', 'email');
 
-              <p className="mb-2 text-2xl font-bold">Registrasi Akun</p>
+      const passwordIsEmpty = userPassword.length < 0;
 
-              <p className="mb-4">
-                Sudah memiliki akun?{' '}
-                <Link href="/login" className="text-slate-500 underline">
-                  Silakan masuk
-                </Link>
-              </p>
+      if (passwordIsEmpty)
+        throw new RegisterError(
+          'Mohon isi password Anda minimal 6 karakter',
+          'password-length'
+        );
 
-              <ErrorMessage showError={showErrorMsg}>{errorMsg}</ErrorMessage>
-            </div>
+      const createdUser = await createUserWithEmailAndPassword(
+        getAuth(),
+        userEmail,
+        userPassword
+      );
 
-            <div className="mx-auto max-w-2xl">
-              <form>
-                <section className="mb-8 grid gap-y-4">
-                  <label htmlFor="email-input" className="inline-block w-full">
-                    <Input
-                      id="email-input"
-                      type="email"
-                      placeholder="Email"
-                      value={userEmail}
-                      onChange={onEmailInput}
-                    />
-                  </label>
+      const signedUserEmail = createdUser.user.email;
+      const dasboardUnamePath = signedUserEmail?.slice(
+        0,
+        signedUserEmail?.indexOf('@')
+      );
 
-                  <label
-                    htmlFor="password-input"
-                    className="inline-block w-full"
-                  >
-                    <Input
-                      id="password-input"
-                      type="password"
-                      placeholder="Password (min 6 karakter)"
-                      value={userPassword}
-                      onChange={onPasswordInput}
-                    />
-                  </label>
-                </section>
+      nextRouter.replace(`/${dasboardUnamePath}`);
+    } catch (error) {
+      if (error instanceof RegisterError && error.cause === 'email') {
+        setErrorMsg(error.message);
+      }
 
-                <section>
-                  <PrimaryButton
-                    disabled={btnDisabledStatus}
-                    onClick={onRegister}
-                  >
-                    Buat akun
-                  </PrimaryButton>
-                </section>
-              </form>
+      if (error instanceof RegisterError && error.cause === 'password-length') {
+        setErrorMsg(error.message);
+      }
+
+      if (
+        error instanceof FirebaseError &&
+        error.code === 'auth/email-already-in-use'
+      ) {
+        setErrorMsg('Maaf email telah digunakan');
+      }
+
+      if (
+        error instanceof FirebaseError &&
+        error.code === 'auth/invalid-email'
+      ) {
+        setErrorMsg('Mohon isi email Anda dengan benar');
+      }
+
+      if (
+        error instanceof FirebaseError &&
+        error.code === 'auth/weak-password'
+      ) {
+        setErrorMsg('Mohon isi password Anda minimal 6 karakter');
+      }
+
+      setShowErrorMsg(() => true);
+    }
+  };
+
+  if (userLogin === true) {
+    return null;
+  } else {
+    return (
+      <main className={`${ralewayFont.variable}`}>
+        <Head>
+          <title>GOVEL | Masuk</title>
+          <meta
+            name="viewport"
+            content="initial-scale=1.0, width=device-width"
+          />
+        </Head>
+
+        <div className="h-screen w-full">
+          <div className="grid h-full items-center">
+            <div>
+              <div className="mb-6 text-center">
+                <h2 className="mb-2 cursor-default select-none font-raleway text-4xl font-bold">
+                  GOVEL
+                </h2>
+
+                <p className="mb-2 text-2xl font-bold">Registrasi Akun</p>
+
+                <p className="mb-4">
+                  Sudah memiliki akun?{' '}
+                  <Link href="/login" className="text-slate-500 underline">
+                    Silakan masuk
+                  </Link>
+                </p>
+
+                <ErrorMessage showError={showErrorMsg}>{errorMsg}</ErrorMessage>
+              </div>
+
+              <div className="mx-auto max-w-2xl">
+                <form>
+                  <section className="mb-8 grid gap-y-4">
+                    <label
+                      htmlFor="email-input"
+                      className="inline-block w-full"
+                    >
+                      <Input
+                        id="email-input"
+                        type="email"
+                        placeholder="Email"
+                        value={userEmail}
+                        onChange={onEmailInput}
+                      />
+                    </label>
+
+                    <label
+                      htmlFor="password-input"
+                      className="inline-block w-full"
+                    >
+                      <Input
+                        id="password-input"
+                        type="password"
+                        placeholder="Password (min 6 karakter)"
+                        value={userPassword}
+                        onChange={onPasswordInput}
+                      />
+                    </label>
+                  </section>
+
+                  <section>
+                    <PrimaryButton
+                      disabled={btnDisabledStatus}
+                      onClick={onRegister}
+                    >
+                      Buat akun
+                    </PrimaryButton>
+                  </section>
+                </form>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </main>
-  );
+      </main>
+    );
+  }
 }
