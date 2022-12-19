015f73f9d9986012715151f42b3cda1cf2a71d49
syahdaromstudenttelu <syahdarom@student.telkomuniversity.ac.id>
2022-12-20 00:13:00 +0700

    feat: update book ticket pages

--- src/pages/book-ticket/date/index.tsx
+++ src/pages/book-ticket/date/index.tsx
@@ -1,34 +1,39 @@
-import type { ChangeEvent } from 'react';
-import { useRouter } from 'next/router';
 import { format } from 'date-fns';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
+import type { ChangeEvent } from 'react';
+import CityOptions from '../../../components/book-ticket-date/CityOptions';
+import ShiftOptions from '../../../components/book-ticket-date/ShiftOptions';
+import { PrimaryButton } from '../../../components/buttons';
+import ErrorMessage from '../../../components/ErrorMessage';
+import ProfileButton from '../../../components/ProfileButton';
+import { interFont, ralewayFont } from '../../../lib/myNextFonts';
+import { useRedirectDashboard } from '../../../lib/react-custom-hooks/useRedirectDashboard';
+import { alertSelector, setAlert } from '../../../redux-app/slices/alertSlice';
+import {
+  bookTicketInputSelector,
+  setBookDate,
+  setBookFrom,
+  setBookShift,
+  setBookSitPos,
+  setBookTo,
+} from '../../../redux-app/slices/bookTicketSlice';
 import {
   useAppDispatch,
   useAppSelector,
 } from '../../../redux-app/typed-hook/typedHooks';
-import {
-  setBookFrom,
-  setBookTo,
-  setBookDate,
-  setBookShift,
-  setBookSitPos,
-} from '../../../redux-app/slices/bookTicketSlice';
-import { bookTicketInputSelector } from '../../../redux-app/slices/bookTicketSlice';
-import { ralewayFont, interFont } from '../../../lib/myNextFonts';
-import Head from 'next/head';
-import Link from 'next/link';
-import ProfileButton from '../../../components/ProfileButton';
-import { PrimaryButton } from '../../../components/buttons';
-import CityOptions from '../../../components/book-ticket-date/CityOptions';
-import ShiftOptions from '../../../components/book-ticket-date/ShiftOptions';
 
 export default function BookTicketDate() {
+  const { username, userLogin } = useRedirectDashboard();
   const nextRouter = useRouter();
   const reduxDispatch = useAppDispatch();
   const { bookFrom, bookTo, bookDate, bookShift } = useAppSelector(
     bookTicketInputSelector
   );
+  const { showAlert, alertMessage } = useAppSelector(alertSelector);
   const disableBookSitBtn =
     bookFrom === '' || bookTo === '' || bookDate === '' || bookShift === '';
+  const disableBookShiftSelector = bookDate === '';
 
   const onInputBookFrom = (e: ChangeEvent<HTMLSelectElement>) => {
     const selectValue = e.currentTarget.value;
@@ -57,151 +62,166 @@ export default function BookTicketDate() {
 
   const onBookSitBtn = () => {
     if (disableBookSitBtn) return;
-    nextRouter.replace(`/book-ticket/sit`);
+    reduxDispatch(setAlert({ alertMessage: '', showAlert: false }));
+    nextRouter.push(`/book-ticket/sit`);
   };
 
   const onClearBookTicket = () => {
+    reduxDispatch(setAlert({ alertMessage: '', showAlert: false }));
     reduxDispatch(setBookFrom('bandung'));
     reduxDispatch(setBookTo('bogor'));
     reduxDispatch(setBookDate(''));
     reduxDispatch(setBookShift(''));
+    nextRouter.push(`/${username}`);
   };
 
-  return (
-    <main className={`${ralewayFont.variable} ${interFont.variable}`}>
-      <Head>
-        <title>GOVEL | Form Pemesanan Tujuan dan Waktu Tiket</title>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      </Head>
+  if (userLogin === false) {
+    return null;
+  } else {
+    return (
+      <main className={`${ralewayFont.variable} ${interFont.variable}`}>
+        <Head>
+          <title>GOVEL | Form Pemesanan Tujuan dan Waktu Tiket</title>
+          <meta
+            name="viewport"
+            content="initial-scale=1.0, width=device-width"
+          />
+        </Head>
 
-      <div className="h-screen w-full">
-        <div className="grid h-full grid-cols-2">
-          <div className="relative h-full">
-            <div
-              className="h-full bg-hero-bg bg-cover bg-right"
-              aria-hidden="true"
-              role="presentation"
-            />
+        <div className="h-screen w-full">
+          <div className="grid h-full grid-cols-2">
+            <div className="relative h-full">
+              <div
+                className="h-full bg-hero-bg bg-cover bg-right"
+                aria-hidden="true"
+                role="presentation"
+              />
 
-            <div className="absolute top-0 left-0 h-full w-full bg-gradient-to-t from-black/80 to-transparent" />
+              <div className="absolute top-0 left-0 h-full w-full bg-gradient-to-t from-black/80 to-transparent" />
 
-            <p className="absolute bottom-4 left-3 text-xs text-slate-400">
-              Gambar milik{' '}
-              <a
-                href="https://unsplash.com/@tristanhess?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
-                className="underline"
-              >
-                Tristan Hess
-              </a>{' '}
-              dari{' '}
-              <a
-                href="https://unsplash.com/s/photos/mini-bus?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
-                className="underline"
-              >
-                Unsplash
-              </a>
-            </p>
-          </div>
+              <p className="absolute bottom-4 left-3 text-xs text-slate-400">
+                Gambar milik{' '}
+                <a
+                  href="https://unsplash.com/@tristanhess?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
+                  className="underline"
+                >
+                  Tristan Hess
+                </a>{' '}
+                dari{' '}
+                <a
+                  href="https://unsplash.com/s/photos/mini-bus?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
+                  className="underline"
+                >
+                  Unsplash
+                </a>
+              </p>
+            </div>
 
-          <section className="h-full">
-            <div className="grid h-full p-4">
-              <div>
-                <div className="mb-16 flex items-center justify-between">
-                  <div>
-                    <Link
-                      className="inline-block rounded-lg bg-slate-200/50 px-2 py-1 transition-colors duration-300 hover:bg-slate-200 hover:outline-none"
-                      href={`/`}
-                      onClick={onClearBookTicket}
-                    >
-                      Tiket Anda
-                    </Link>
+            <section className="h-full">
+              <div className="grid h-full p-4">
+                <div>
+                  <div className="mb-16 flex items-center justify-between">
+                    <div>
+                      <button
+                        className="inline-block rounded-lg bg-slate-200/50 px-2 py-1 transition-colors duration-300 hover:bg-slate-200 hover:outline-none"
+                        onClick={onClearBookTicket}
+                        disabled={username === ''}
+                      >
+                        Tiket Anda
+                      </button>
+                    </div>
+
+                    <ProfileButton />
                   </div>
 
-                  <ProfileButton />
-                </div>
+                  <div className="mb-8 min-h-[142px] text-center">
+                    <h1 className="mb-1 cursor-default select-none font-raleway text-6xl font-bold text-slate-700">
+                      GOVEL
+                    </h1>
 
-                <div className="mb-8 text-center">
-                  <h1 className="mb-1 cursor-default select-none font-raleway text-6xl font-bold text-slate-700">
-                    GOVEL
-                  </h1>
+                    <p className="mb-4 text-lg font-semibold">
+                      Yukk bepergian dengan Go Travel!
+                    </p>
 
-                  <p className="text-lg font-semibold">
-                    Yukk bepergian dengan Go Travel!
-                  </p>
-                </div>
+                    <ErrorMessage showError={showAlert}>
+                      {alertMessage}
+                    </ErrorMessage>
+                  </div>
 
-                <div>
-                  <form className="mx-auto max-w-md">
-                    <section className="mb-6 grid min-h-[200px] gap-y-4">
-                      <div className="flex items-center justify-between">
-                        <label htmlFor="from-city">
-                          <p>Kota asal</p>
+                  <div>
+                    <form className="mx-auto max-w-md">
+                      <section className="mb-6 grid min-h-[200px] gap-y-4">
+                        <div className="flex items-center justify-between">
+                          <label htmlFor="from-city">
+                            <p>Kota asal</p>
 
-                          <select
-                            id="from-city"
-                            value={bookFrom}
-                            onChange={onInputBookFrom}
-                          >
-                            <CityOptions fromOrTo="from" />
-                          </select>
-                        </label>
+                            <select
+                              id="from-city"
+                              value={bookFrom}
+                              onChange={onInputBookFrom}
+                            >
+                              <CityOptions fromOrTo="from" />
+                            </select>
+                          </label>
 
-                        <label htmlFor="to-city">
-                          <p>Kota tujuan</p>
-                          <select
-                            id="to-city"
-                            value={bookTo}
-                            onChange={onInputBookTo}
-                          >
-                            <CityOptions fromOrTo="to" />
-                          </select>
-                        </label>
-                      </div>
+                          <label htmlFor="to-city">
+                            <p>Kota tujuan</p>
+                            <select
+                              id="to-city"
+                              value={bookTo}
+                              onChange={onInputBookTo}
+                            >
+                              <CityOptions fromOrTo="to" />
+                            </select>
+                          </label>
+                        </div>
 
-                      <label
-                        htmlFor="ticket-date"
-                        className="inline-block w-full"
-                      >
-                        <p>Pilih tanggal tiket Anda</p>
-                        <input
-                          type="date"
-                          id="ticket-date"
-                          min={format(new Date(), 'yyyy-MM-dd')}
-                          value={bookDate}
-                          onChange={onInputBookDate}
-                        />
-                      </label>
-
-                      <label
-                        htmlFor="ticket-shift"
-                        className="inline-block w-full"
-                      >
-                        <p>Pilih shift tiket Anda</p>
-                        <select
-                          id="ticket-shift"
-                          value={bookShift}
-                          onChange={onInputBookShift}
+                        <label
+                          htmlFor="ticket-date"
+                          className="inline-block w-full"
                         >
-                          <ShiftOptions />
-                        </select>
-                      </label>
-                    </section>
+                          <p>Pilih tanggal tiket Anda</p>
+                          <input
+                            type="date"
+                            id="ticket-date"
+                            min={format(new Date(), 'yyyy-MM-dd')}
+                            value={bookDate}
+                            onChange={onInputBookDate}
+                          />
+                        </label>
 
-                    <section>
-                      <PrimaryButton
-                        disabled={disableBookSitBtn}
-                        onClick={onBookSitBtn}
-                      >
-                        Pilih kursi
-                      </PrimaryButton>
-                    </section>
-                  </form>
+                        <label
+                          htmlFor="ticket-shift"
+                          className="inline-block w-full"
+                        >
+                          <p>Pilih shift tiket Anda</p>
+                          <select
+                            id="ticket-shift"
+                            value={bookShift}
+                            onChange={onInputBookShift}
+                            disabled={disableBookShiftSelector}
+                          >
+                            <ShiftOptions />
+                          </select>
+                        </label>
+                      </section>
+
+                      <section>
+                        <PrimaryButton
+                          disabled={disableBookSitBtn}
+                          onClick={onBookSitBtn}
+                        >
+                          Pilih kursi
+                        </PrimaryButton>
+                      </section>
+                    </form>
+                  </div>
                 </div>
               </div>
-            </div>
-          </section>
+            </section>
+          </div>
         </div>
-      </div>
-    </main>
-  );
+      </main>
+    );
+  }
 }
--- src/pages/book-ticket/sit/index.tsx
+++ src/pages/book-ticket/sit/index.tsx
@@ -1,44 +1,57 @@
-import type { ChangeEvent } from 'react';
+import Head from 'next/head';
+import Link from 'next/link';
 import { useRouter } from 'next/router';
+import type { ChangeEvent } from 'react';
+import { useEffect, useState } from 'react';
+import SitOptions from '../../../components/book-ticket-sit/SitOptions';
+import { SecondaryButtonLink } from '../../../components/button-links';
+import { PrimaryButton } from '../../../components/buttons';
+import ErrorMessage from '../../../components/ErrorMessage';
+import ProfileButton from '../../../components/ProfileButton';
+import { interFont, ralewayFont } from '../../../lib/myNextFonts';
+import { useRedirectDashboard } from '../../../lib/react-custom-hooks/useRedirectDashboard';
+import { useTicketAvailability } from '../../../lib/react-custom-hooks/useTicketAvailability';
+import { alertSelector, setAlert } from '../../../redux-app/slices/alertSlice';
+import {
+  bookTicketInputSelector,
+  setBookDate,
+  setBookFrom,
+  setBookShift,
+  setBookSitPos,
+  setBookTo,
+} from '../../../redux-app/slices/bookTicketSlice';
+import type { TicketSitPos } from '../../../redux-app/slices/userBookedTicketsSlice';
 import {
   useAppDispatch,
   useAppSelector,
 } from '../../../redux-app/typed-hook/typedHooks';
-import {
-  setBookFrom,
-  setBookTo,
-  setBookDate,
-  setBookShift,
-  setBookSitPos,
-} from '../../../redux-app/slices/bookTicketSlice';
-import { bookTicketInputSelector } from '../../../redux-app/slices/bookTicketSlice';
-import { ralewayFont, interFont } from '../../../lib/myNextFonts';
-import Head from 'next/head';
-import Link from 'next/link';
-import ProfileButton from '../../../components/ProfileButton';
-import { PrimaryButton } from '../../../components/buttons';
-import { SecondaryButtonLink } from '../../../components/button-links';
-import SitOptions from '../../../components/book-ticket-sit/SitOptions';
-
-type TicketSit = 1 | 2 | 3 | 4 | 5;
 
 export default function BookTicketSit() {
+  const [redirectOnce, setRedirectOnce] = useState(false);
+  const { updateTicketAvailability } = useTicketAvailability();
+  const { username, userLogin } = useRedirectDashboard();
   const nextRouter = useRouter();
   const reduxDispatch = useAppDispatch();
-  const { bookSitPos } = useAppSelector(bookTicketInputSelector);
+  const { bookDate, bookShift, bookSitPos } = useAppSelector(
+    bookTicketInputSelector
+  );
+  const { showAlert, alertMessage } = useAppSelector(alertSelector);
   const disableBookSitBtn = bookSitPos === null;
+  const bookingTicketDateIsEmpty = bookDate === '' || bookShift === '';
 
   const onInputBookSit = (e: ChangeEvent<HTMLSelectElement>) => {
-    const selectValue = Number.parseInt(e.currentTarget.value) as TicketSit;
+    const selectValue = Number.parseInt(e.currentTarget.value) as TicketSitPos;
     reduxDispatch(setBookSitPos(selectValue));
   };
 
   const onBookNextBtn = () => {
     if (disableBookSitBtn) return;
+    reduxDispatch(setAlert({ alertMessage: '', showAlert: false }));
     nextRouter.push(`/book-ticket/passanger-data`);
   };
 
   const onClearBookTicket = () => {
+    reduxDispatch(setAlert({ alertMessage: '', showAlert: false }));
     reduxDispatch(setBookFrom('bandung'));
     reduxDispatch(setBookTo('bogor'));
     reduxDispatch(setBookDate(''));
@@ -46,112 +59,143 @@ export default function BookTicketSit() {
     reduxDispatch(setBookSitPos(null));
   };
 
-  return (
-    <main className={`${ralewayFont.variable} ${interFont.variable}`}>
-      <Head>
-        <title>GOVEL | Form Pemesanan Kursi Tiket</title>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      </Head>
+  useEffect(() => {
+    if (redirectOnce) return;
+    if (userLogin === false) return;
+    if (bookingTicketDateIsEmpty) {
+      const redirect = async () => {
+        await updateTicketAvailability();
+        nextRouter.replace('/book-ticket/date');
+      };
+      redirect();
+    }
 
-      <div className="h-screen w-full">
-        <div className="grid h-full grid-cols-2">
-          <div className="relative h-full">
-            <div
-              className="h-full bg-hero-bg bg-cover bg-right"
-              aria-hidden="true"
-              role="presentation"
-            />
+    setRedirectOnce(true);
+  }, [
+    bookingTicketDateIsEmpty,
+    nextRouter,
+    redirectOnce,
+    updateTicketAvailability,
+    userLogin,
+  ]);
 
-            <div className="absolute top-0 left-0 h-full w-full bg-gradient-to-t from-black/80 to-transparent" />
+  if (userLogin === false || bookingTicketDateIsEmpty) {
+    return null;
+  } else {
+    return (
+      <main className={`${ralewayFont.variable} ${interFont.variable}`}>
+        <Head>
+          <title>GOVEL | Form Pemesanan Kursi Tiket</title>
+          <meta
+            name="viewport"
+            content="initial-scale=1.0, width=device-width"
+          />
+        </Head>
 
-            <p className="absolute bottom-4 left-3 text-xs text-slate-400">
-              Gambar milik{' '}
-              <a
-                href="https://unsplash.com/@tristanhess?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
-                className="underline"
-              >
-                Tristan Hess
-              </a>{' '}
-              dari{' '}
-              <a
-                href="https://unsplash.com/s/photos/mini-bus?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
-                className="underline"
-              >
-                Unsplash
-              </a>
-            </p>
-          </div>
+        <div className="h-screen w-full">
+          <div className="grid h-full grid-cols-2">
+            <div className="relative h-full">
+              <div
+                className="h-full bg-hero-bg bg-cover bg-right"
+                aria-hidden="true"
+                role="presentation"
+              />
 
-          <section className="h-full">
-            <div className="grid h-full p-4">
-              <div>
-                <div className="mb-16 flex items-center justify-between">
-                  <div>
-                    <Link
-                      className="inline-block rounded-lg bg-slate-200/50 px-2 py-1 transition-colors duration-300 hover:bg-slate-200 hover:outline-none"
-                      href={`/`}
-                      onClick={onClearBookTicket}
-                    >
-                      Tiket Anda
-                    </Link>
+              <div className="absolute top-0 left-0 h-full w-full bg-gradient-to-t from-black/80 to-transparent" />
+
+              <p className="absolute bottom-4 left-3 text-xs text-slate-400">
+                Gambar milik{' '}
+                <a
+                  href="https://unsplash.com/@tristanhess?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
+                  className="underline"
+                >
+                  Tristan Hess
+                </a>{' '}
+                dari{' '}
+                <a
+                  href="https://unsplash.com/s/photos/mini-bus?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
+                  className="underline"
+                >
+                  Unsplash
+                </a>
+              </p>
+            </div>
+
+            <section className="h-full">
+              <div className="grid h-full p-4">
+                <div>
+                  <div className="mb-16 flex items-center justify-between">
+                    <div>
+                      <Link
+                        className="inline-block rounded-lg bg-slate-200/50 px-2 py-1 transition-colors duration-300 hover:bg-slate-200 hover:outline-none"
+                        href={`/${username}`}
+                        onClick={onClearBookTicket}
+                      >
+                        Tiket Anda
+                      </Link>
+                    </div>
+
+                    <ProfileButton />
                   </div>
 
-                  <ProfileButton />
-                </div>
+                  <div className="mb-6 min-h-[142px] text-center">
+                    <h1 className="mb-1 cursor-default select-none font-raleway text-6xl font-bold text-slate-700">
+                      GOVEL
+                    </h1>
 
-                <div className="mb-8 text-center">
-                  <h1 className="mb-1 cursor-default select-none font-raleway text-6xl font-bold text-slate-700">
-                    GOVEL
-                  </h1>
+                    <p className="mb-4 text-lg font-semibold">
+                      Yukk bepergian dengan Go Travel!
+                    </p>
 
-                  <p className="text-lg font-semibold">
-                    Yukk bepergian dengan Go Travel!
-                  </p>
-                </div>
+                    <ErrorMessage showError={showAlert}>
+                      {alertMessage}
+                    </ErrorMessage>
+                  </div>
 
-                <div>
-                  <form className="mx-auto max-w-md">
-                    <section className="mb-6 min-h-[200px]">
-                      <label
-                        htmlFor="from-city"
-                        className="inline-block w-full"
-                      >
-                        <p className="mb-6 text-center text-2xl font-semibold">
-                          Pilih Kursi
-                        </p>
-
-                        <select
-                          id="from-city"
-                          className="inline-block w-full px-3 py-3"
-                          value={bookSitPos === null ? '' : bookSitPos}
-                          onChange={onInputBookSit}
+                  <div>
+                    <form className="mx-auto max-w-md">
+                      <section className="mb-6 min-h-[200px]">
+                        <label
+                          htmlFor="from-city"
+                          className="inline-block w-full"
                         >
-                          <SitOptions />
-                        </select>
-                      </label>
-                    </section>
+                          <p className="mb-6 text-center text-2xl font-semibold">
+                            Pilih Kursi
+                          </p>
 
-                    <section>
-                      <div className="mb-2">
-                        <PrimaryButton
-                          disabled={disableBookSitBtn}
-                          onClick={onBookNextBtn}
-                        >
-                          Lanjut
-                        </PrimaryButton>
-                      </div>
+                          <select
+                            id="from-city"
+                            className="inline-block w-full px-3 py-3"
+                            value={bookSitPos === null ? '' : bookSitPos}
+                            onChange={onInputBookSit}
+                          >
+                            <SitOptions />
+                          </select>
+                        </label>
+                      </section>
 
-                      <SecondaryButtonLink hrefPath={`/book-ticket/date`}>
-                        Kembali
-                      </SecondaryButtonLink>
-                    </section>
-                  </form>
+                      <section>
+                        <div className="mb-2">
+                          <PrimaryButton
+                            disabled={disableBookSitBtn}
+                            onClick={onBookNextBtn}
+                          >
+                            Lanjut
+                          </PrimaryButton>
+                        </div>
+
+                        <SecondaryButtonLink hrefPath={`/book-ticket/date`}>
+                          Kembali
+                        </SecondaryButtonLink>
+                      </section>
+                    </form>
+                  </div>
                 </div>
               </div>
-            </div>
-          </section>
+            </section>
+          </div>
         </div>
-      </div>
-    </main>
-  );
+      </main>
+    );
+  }
 }
